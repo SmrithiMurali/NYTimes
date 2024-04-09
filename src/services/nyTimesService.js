fb9bd5b865e2ca19b6ec28eadaa08098ceae7b8b
Smrithi <smrithi@Smrithis-MacBook-Air.local>
2024-04-09 22:28:30 +0530

    add async function

--- src/services/nyTimesService.js
+++ src/services/nyTimesService.js
@@ -1,5 +1,5 @@
 const apiCalls = {
-  getMostPopularArticles() {
+  async getMostPopularArticles() {
     const apiKey = "D1NckhzDRRGYUAewi47GHYUlwAlJQNGT";
     return fetch(
       `https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=${apiKey}`
